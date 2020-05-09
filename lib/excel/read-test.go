fdc231a1398fa93268a7074fa7182e535110c651
DAI ANDO <daifloss@gmail.com>
2020-05-09 11:27:07 +0900

    read txt test

--- lib/excel/read-test.go
+++ lib/excel/read-test.go
@@ -1,1 +1,17 @@
 package excel
+
+// read excel and print cell contents
+import (
+	"fmt"
+	"io/ioutil"
+)
+
+// Test read test
+func Test() {
+	data, err := ioutil.ReadFile("test.txt")
+	if err != nil {
+		fmt.Println("File reading error", err)
+		return
+	}
+	fmt.Println("Contents of file:", string(data))
+}
